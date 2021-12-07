5b433966d8206ede1d57df12c5f29be93f5e26fc
Angrymouse <rykov.nikas@gmail.com>
2021-12-07 05:40:39 +0200

    req url

--- routes/root.js
+++ routes/root.js
@@ -36,7 +36,7 @@ module.exports = async function (fastify, opts) {
 		let hnsName =
 			hostname.slice(0, hostname.length - targetDomain.length) + //"-1" means remove "."
 			config.domainMap[targetDomain];
-		console.log(hnsName);
+		console.log(request.url);
 		if (hnsName == ".") {
 			if (request.url == "/analytics") {
 				if (analyticsEnabled) {
