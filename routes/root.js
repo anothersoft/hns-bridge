3f3b81c004917b69f6ab4d5b04eefb23df14021b
Angrymouse <rykov.nikas@gmail.com>
2022-02-14 04:42:58 +0200

    test

--- routes/root.js
+++ routes/root.js
@@ -1,16 +1,20 @@
 const http = require("http");
 const dns = require("dns").promises;
-
+const fetch = require("node-fetch");
 const CacheableLookup = require("cacheable-lookup");
 const config = require("../config.json");
 const cacheable = new CacheableLookup();
 const fs = require("fs");
 cacheable.install(http.globalAgent);
+let mainPortal = config.siaPortals[0];
+
 cacheable.servers = config.nameservers;
 dns.setServers(config.nameservers);
 // dns.setServers(config.nameservers);
 let path = require("path");
-
+setInterval(async () => {
+	mainPortal = await getAlivePortal();
+}, 100000);
 module.exports = async function (fastify, opts) {
 	fastify.addContentTypeParser(
 		"*",
@@ -25,67 +29,136 @@ module.exports = async function (fastify, opts) {
 		}
 	);
 
-	fastify.all("*", async function (request, reply) {
-		let hostname = request.hostname; //I really need it for debug, don't remove and make PR!!!
-		let domainMapArray = Object.keys(config.domainMap);
-		domainMapArray.sort((a, b) => b.length - a.length);
-		let targetDomain = domainMapArray.find((domain) =>
-			hostname.endsWith(domain)
-		);
-		if (!hostname || !targetDomain) {
-			return reply.redirect(301, require("../config.json").rootRedirect);
-		}
-		let hnsName =
-			hostname.slice(0, hostname.length - targetDomain.length) + //"-1" means remove "."
-			config.domainMap[targetDomain];
-
-		if (hnsName == "") {
-			if (config.defaultDomain) {
-				hnsName = config.defaultDomain;
-			} else {
+	fastify.all("*", function (request, reply) {
+		(async () => {
+			let hostname = request.hostname; //I really need it for debug, don't remove and make PR!!!
+			let domainMapArray = Object.keys(config.domainMap);
+			domainMapArray.sort((a, b) => b.length - a.length);
+			let targetDomain = domainMapArray.find((domain) =>
+				hostname.endsWith(domain)
+			);
+			if (!hostname || !targetDomain) {
 				return reply.redirect(301, require("../config.json").rootRedirect);
 			}
-		}
-		let headers = request.headers;
+			let hnsName =
+				hostname.slice(0, hostname.length - targetDomain.length) + //"-1" means remove "."
+				config.domainMap[targetDomain];
 
-		delete headers.host;
-		if (config.siaNative) {
-			console.log(await dns.resolveTxt("_contenthash." + hnsName));
-		}
-		try {
-			let resource = http.request(
-				{
-					hostname: hnsName,
-					headers: headers,
-					lookup: cacheable.lookup,
-
-					method: request.method,
-					path: request.url,
-				},
-				(res) => {
-					reply.raw.writeHead(res.statusCode, res.headers);
-
-					res.on("data", (data) => {
-						reply.raw.write(data);
-					});
-					res.on("end", () => {
-						reply.raw.end();
-					});
+			if (hnsName == "") {
+				if (config.defaultDomain) {
+					hnsName = config.defaultDomain;
+				} else {
+					return reply.redirect(301, require("../config.json").rootRedirect);
 				}
-			);
-
-			resource.once("error", () => {
-				return reply.redirect(
-					302,
-					"https://www.namebase.io/domains/" + hnsName
-				);
-			});
-			if (["PUT", "PATCH", "POST"].includes(request.method)) {
-				resource.write(request.body);
 			}
-			resource.end();
-		} catch (error) {
-			return reply.redirect(302, "https://www.namebase.io/domains/" + hnsName);
-		}
+			let headers = request.headers;
+
+			delete headers.host;
+			if (config.siaNative) {
+				dns
+					.resolveTxt("_contenthash." + hnsName)
+					.then((records) => {
+						if (records.length < 1) {
+							processNormally(hnsName, headers, request, reply);
+						} else {
+							let siaRecord = records[0][0];
+							if (!siaRecord || !siaRecord.startsWith("sia://")) {
+								processNormally(hnsName, headers, request, reply);
+							} else {
+								processSia(siaRecord, request, reply);
+							}
+						}
+					})
+					.catch((err) => {
+						processNormally(hnsName, headers, request, reply);
+						return;
+					});
+			} else {
+				processNormally(hnsName, headers, request, reply);
+			}
+		})();
 	});
 };
+function processNormally(hnsName, headers, request, reply) {
+	try {
+		let resource = http.request(
+			{
+				hostname: hnsName,
+				headers: headers,
+				lookup: cacheable.lookup,
+
+				method: request.method,
+				path: request.url,
+			},
+			(res) => {
+				reply.raw.writeHead(res.statusCode, res.headers);
+
+				res.on("data", (data) => {
+					reply.raw.write(data);
+				});
+				res.on("end", () => {
+					reply.raw.end();
+				});
+			}
+		);
+
+		resource.once("error", () => {
+			return reply.redirect(302, "https://www.namebase.io/domains/" + hnsName);
+		});
+		if (["PUT", "PATCH", "POST"].includes(request.method)) {
+			resource.write(request.body);
+		}
+		resource.end();
+	} catch (error) {
+		return reply.redirect(302, "https://www.namebase.io/domains/" + hnsName);
+	}
+}
+
+async function processSia(siaLink, request, reply) {
+	try {
+		let resource = await fetch(
+			mainPortal + siaLink.slice("sia://".length + request.url),
+			{ headers: { "User-agent": "Sia-Agent" } }
+		);
+
+		resource.once("error", () => {
+			return reply.redirect(302, "https://www.namebase.io/domains/" + hnsName);
+		});
+		resource.pipe(reply);
+	} catch (error) {
+		return reply.redirect(302, "https://www.namebase.io/domains/" + hnsName);
+	}
+}
+async function getAlivePortal() {
+	return new Promise(async (resolve, reject) => {
+		let found = false;
+		config.siaPortals.forEach((portal, index) => {
+			setTimeout(async () => {
+				if (found) {
+					return;
+				} else {
+					try {
+						let resource = await (
+							await fetch(
+								portal + "AACo6KGldohjcBP39JUxFEWWMwiTTV_wQfrd5z28gUoYxA",
+								{
+									headers: {
+										"User-agent": "Sia-Agent",
+									},
+								}
+							)
+						).text();
+						if (resource == "test") {
+							found = true;
+							resolve(portal);
+						} else {
+							return;
+						}
+					} catch (e) {
+						return;
+					}
+				}
+			}, index * 200);
+		});
+	});
+}
