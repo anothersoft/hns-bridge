2c22aba822f34bd4a16cac0202bb80ddf58cb750
Angrymouse <rykov.nikas@gmail.com>
2022-02-15 17:54:52 +0200

    headers

--- routes/root.js
+++ routes/root.js
@@ -137,7 +137,7 @@ async function processSia(siaLink, request, reply) {
 			mainPortal + siaLink.slice("sia://".length) + request.url,
 			{ headers: { "User-agent": "Sia-Agent" } }
 		);
-		console.log(resource);
+		console.log(resource.headers);
 		reply.raw.writeHead(resource.status, resource.headers);
 		resource.body.pipe(reply.raw);
 	} catch (error) {
